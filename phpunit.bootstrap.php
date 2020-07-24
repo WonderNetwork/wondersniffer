b7cd511652f217b6dccb5fdf94c130577f316647
Gemma Anible <gemma@wonderproxy.com>
2020-07-24 18:37:48 +0200

    upgrade phpcs and fix the autoloader

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -1,6 +1,17 @@
 <?php
 
-require_once __DIR__ .'/vendor/autoload.php';
+if (defined('PHP_CODESNIFFER_IN_TESTS') === false) {
+    define('PHP_CODESNIFFER_IN_TESTS', true);
+}
 
-// Load Test Helper
-require_once __DIR__ . '/TestHelper.php';
+if (defined('PHP_CODESNIFFER_CBF') === false) {
+    define('PHP_CODESNIFFER_CBF', false);
+}
+
+if (defined('PHP_CODESNIFFER_VERBOSITY') === false) {
+    define('PHP_CODESNIFFER_VERBOSITY', 0);
+}
+
+require_once __DIR__ .'/vendor/squizlabs/php_codesniffer/autoload.php';
+
+require_once __DIR__ .'/vendor/autoload.php';
